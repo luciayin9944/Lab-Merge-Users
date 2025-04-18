62eddda0c599a34fce8b0d403409c0252b9c5f51
luciayin9944 <luxiayin94@gmail.com>
2025-04-17 21:50:49 -0700

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  // Merge all user arrays into one using spread operator
+  for (const userArray of args) {
+    combinedObject.users.push(...userArray);
+  }
+
+  // Add merge_date in M/d/yyyy format
+  const currentDate = new Date();
+  combinedObject.merge_date = currentDate.toString('M/d/yyyy');
+
+  return combinedObject;
+}
+``
+
+
+
 
 
 
